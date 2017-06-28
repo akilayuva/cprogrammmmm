c434e3764d1c452ec77fbd4f1001cc71b1890100
akilayuva <akilapadma97@gmail.com>
2017-06-28 12:26:12 +0530

    Create sumpro.c

---
+++ sumpro.c
@@ -0,0 +1,13 @@
+#include<stdio.h<>
+int main()
+{
+int i,n,sum=0;
+printf("Enter the N number");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum +=i;
+}
+printf("The sum of N number is %d",sum);
+return 0;
+}
